5465069861c7284f0479f9f6914ac75b0d27df22
umayer10-AI <mdumayer10@gmail.com>
2026-04-26 17:48:37 +0600

    add

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,29 +1,46 @@
+"use client"
 import Navbar from '@/component/Navbar';
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa6';
 
-const page = () => {
+const Loginpage = () => {
+
+    const {register,handleSubmit,watch,formState: { errors },} = useForm()
+
+    const [s, setSt] = useState(false)
+
+    const handleClick = (v) => {
+        console.log(v)
+    }
+
+    console.log(errors)
+
     return (
         <div>
             <Navbar></Navbar>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(handleClick)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
                     <label className="label text-black font-semibold text-base">Email address</label>
-                    <input type="email" className="input w-full" placeholder="Enter email address" />
+                    <input type="email" className="input w-full" placeholder="Enter email address" {...register("email", { required: "Email filed required" })} />
+                    {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Enter your password" />
+                    <input type={s ? "text" : "password"} className="input w-full" placeholder="Enter your password" {...register("password", { required: "Password filed required" })} />
+                    <span onClick={() => setSt(!s)}>{s ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</span>
+                    {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
 
                     <button className="btn btn-neutral my-4">Login</button>
 
-                    <p className='text-center font-semibold text-base'>Dont’t Have An Account ? <span className='
-                    text-red-600'>Register</span></p>
+                    <p className='text-center font-semibold text-base'>Dont’t Have An Account ? <Link href={'/register'} className='text-red-600'>Register</Link></p>
                 </form>
             </div>
         </div>
     );
 };
 
-export default page;
+export default Loginpage;
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,26 +1,45 @@
+"use client"
 import Navbar from '@/component/Navbar';
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa6';
 
-const page = () => {
+const Registerpage = () => {
+
+    const {register,handleSubmit,watch,formState: { errors },} = useForm()
+
+    const [s, setSt] = useState(false)
+
+    const handleClick = (v) => {
+        console.log(v)
+    }
+
+    
+
     return (
         <div>
             <Navbar></Navbar>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(handleClick)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
                     <label className="label text-black font-semibold text-base">Your Name</label>
-                    <input type="text" className="input w-full" placeholder="Enter your name" />
+                    <input type="text" className="input w-full" placeholder="Enter your name" {...register("name", { required: "Name filed required" })} />
+                    {errors.name && <p className='text-red-600'>{errors.name.message}</p>}
 
                     <label className="label text-black font-semibold text-base">Photo URL</label>
-                    <input type="text" className="input w-full" placeholder="Enter your url" />
+                    <input type="text" className="input w-full" placeholder="Enter your url" {...register("url", { required: "Url filed required" })} />
+                    {errors.url && <p className='text-red-600'>{errors.url.message}</p>}
 
-                    <label className="label text-black font-semibold text-base">Email</label>
-                    <input type="email" className="input w-full" placeholder="Email" />
-
+                    <label className="label text-black font-semibold text-base">Email address</label>
+                    <input type="email" className="input w-full" placeholder="Enter email address" {...register("email", { required: "Email filed required" })} />
+                    {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
+                    
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Password" />
+                    <input type={s ? "text" : "password"} className="input w-full" placeholder="Enter your password" {...register("password", { required: "Password filed required" })} />
+                    <span onClick={() => setSt(!s)}>{s ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</span>
+                    {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
 
                     <label className="label text-black mt-2 font-semibold"><input type="checkbox" className="checkbox checkbox-info" />Accept Term & Conditions</label>
 
@@ -31,4 +50,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default Registerpage;
